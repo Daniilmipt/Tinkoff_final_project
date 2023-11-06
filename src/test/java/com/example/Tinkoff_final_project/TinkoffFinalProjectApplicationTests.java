c2b3e27b4dd60b488e56cf426fb8f61a065b200a
Daniilmipt <sadovnikov.diu@phystech.edu>
2023-11-06 22:07:31 +0300

    create project

---
+++ src/test/java/com/example/Tinkoff_final_project/TinkoffFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tinkoff_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
